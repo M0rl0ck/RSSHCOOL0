d579ac36c262c7dff44960ec7ed81c43a2f3c0b4
M0rl0ck <seriy17@yandex.ru>
2022-01-22 22:34:55 +0300

    fix: fix adapive

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,13 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
 const adaptivemenu = document.querySelector('.nav');
-const allLincs = document.querySelectorAll('.nav-link');
+
 const openMenu = () => {
     hamburger.classList.toggle('hamburger-active');
     adaptivemenu.classList.toggle('open-menu');
 }
-const closeMenu = () => {
-    hamburger.classList.remove('hamburger-active');
-    adaptivemenu.classList.remove('open-menu');
+const closeMenu = (event) => {
+    if (event.target.classList.contains('nav-link')) {
+        hamburger.classList.remove('hamburger-active');
+        adaptivemenu.classList.remove('open-menu');
+    }
 }
+
 hamburger.addEventListener('click', openMenu);
-allLincs.forEach((element) => element.addEventListener('click', closeMenu));
+adaptivemenu.addEventListener('click', closeMenu);
