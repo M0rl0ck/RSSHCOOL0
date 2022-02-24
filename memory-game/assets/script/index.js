244cad3d17f2420f3143818aded19732cc65370b
M0rlock <seriy17@yandex.ru>
2022-02-24 22:24:34 +0300

    add gamelogik

--- memory-game/assets/script/index.js
+++ memory-game/assets/script/index.js
@@ -1,7 +1,54 @@
 const cards = document.querySelectorAll(".cards");
+let isRotatedCard = false;
+let lockCards = false;
+let firstCard, secondCard;
 
-const rotateCard = (e) => {
-  e.currentTarget.classList.toggle("rotate");
-};
+function compareCards() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    return;
+  }
+  unrotateCard();
+}
 
+function disableCards() {
+  isRotatedCard = false;
+  lockCards = false;
+  firstCard.removeEventListener("click", rotateCard);
+  secondCard.removeEventListener("click", rotateCard);
+}
+
+function unrotateCard() {
+  isRotatedCard = false;
+  setTimeout(() => {
+    lockCards = false;
+    firstCard.classList.remove("rotate");
+    secondCard.classList.remove("rotate");
+  }, 1500);
+}
+
+function rotateCard() {
+  if (lockCards) {
+    return;
+  }
+  if (!isRotatedCard) {
+    isRotatedCard = true;
+    firstCard = this;
+    this.classList.add("rotate");
+    return;
+  } else if (firstCard === this) {
+    return;
+  }
+  this.classList.add("rotate");
+  secondCard = this;
+  lockCards = true;
+  compareCards();
+}
+
+function mixCards() {
+  cards.forEach((item) => {
+    item.style.order = Math.floor(Math.random() * 20);
+  });
+}
 cards.forEach((item) => item.addEventListener("click", rotateCard));
+mixCards();
